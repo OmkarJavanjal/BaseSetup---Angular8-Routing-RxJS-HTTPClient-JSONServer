8fc202e2f09e1095dd6fd8157be59d94554c77ae
OmkarJavanjal <a96738@gmail.com>
2019-10-12 13:02:20 +0530

    created generic service

--- src/environments/urlFactory/appWeatherURLs.ts
+++ src/environments/urlFactory/appWeatherURLs.ts
@@ -4,8 +4,10 @@ export class AppWeatherURLs extends AppBase {
 
     getUrls() {
       return {
-        getForecastUrl: this.getBaseUrl() + 'forecast?q=',
-        getForecastUrl2: this.getMockUrl() + 'forecast'
+        // getForecastUrl: this.getBaseUrl() + 'forecast?q=',
+        // getForecastUrl2: this.getMockUrl() + 'forecast'
+        getForecastUrl: {url: this.getBaseUrl() + 'forecast?q=', httpMethod: 'GET2'},
+        getForecastUrl2: {url: this.getMockUrl() + 'forecast', httpMethod: 'GET'}
       };
     }
   
